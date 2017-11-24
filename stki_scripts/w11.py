a682005505364804fdc8695c87aa683d49a05923
muhammadsasmito <muhammad.sasmito.a.w@gmail.com>
2017-11-24 14:33:57 +0700

    tfidf /w cosine

--- stki_scripts/w11.py
+++ stki_scripts/w11.py
@@ -1,4 +1,4 @@
-import math, w3, w4, w6, os
+import math, w3, w4, w6, os, cosine
 from pprint import pprint
 import tfidf
 
@@ -35,6 +35,37 @@ def dob(path):
     # matrix_akhir = w4.matrix(list_of_bow,normalized=True) # jalankan fungsi matrix ke list_of_bow
     return dict_of_bow
 
+def article_limit(path):
+    this_path = os.path.split(__file__)[0]
+    path = os.path.join(this_path, path)
+
+    # membaca sekaligus pre-processing semua artikel simpan ke dictionary
+    articles = {}
+    for item in os.listdir(path):
+        if item.endswith(".txt"):
+            with open(path + "/" + item, 'r') as file:
+                articles[item] = w3.preprotext(file.read())
+
+    # representasi bow
+    list_of_bow = [] # membuat list kosong
+    dict_of_bow = {} # membuat dict kosong
+    for key, value in articles.items(): # iterasi pasangan key, value
+        # print key, value
+        list_token = value.split() # cari kata2 dengan men-split
+        dic = w4.bow(list_token)   # membuat bow
+        # dict_of_bow[key] = list_of_bow.append(dic)    # append bow ke list kosong yg di atas
+        # dict_of_bow[key] = dic
+        dic = w4.sortdic(dic,descending=True)
+        dic_bow = {}
+        # print dic 
+        for keys,values in dic:
+            if values > 3: # dengan bobot setiap kata yang dihitung lebih dari 3 
+                dic_bow[keys] = values
+        dict_of_bow[key] = dic_bow
+    # membuat matrix
+    # matrix_akhir = w4.matrix(list_of_bow,normalized=True) # jalankan fungsi matrix ke list_of_bow
+    return dict_of_bow
+
 def text_exam():
     text_dic = {
         'd1' : 'saya yakin dia tiada mampu menolak saya',
@@ -55,16 +86,43 @@ def text_exam():
     return dobs
 
 def test_similarity(dict_document,keywod):
+    keywod = w3.prepro_base(keywod).split()
+    print keywod
     table = tfidf.TfIdf()
     for key,value in dict_document.items():
          table.add_document(key, value)
-    # return table.similarities(keywod)
-    return w4.sortdic(table.similarities(keywod),descending=True)
+    return table.similarities(keywod)
+    # return w4.sortdic(table.similarities(keywod),descending=True)
 
 def findSim(keyword,pathcorpus):
-    return test_similarity(dob(path),keyword)
+    res_tf = test_similarity(dob(path),keyword) # Hasil tf idf
 
+    keyword = w3.prepro_base(keyword) # prepo kata kuncinya dan artikel
+    articles = article_limit(pathcorpus)
+    # doct = dob(pathcorpus) # artikel/document yang utuh
+
+    # Hitung jarak Jarak Cosine hasil dari tfidf
+    simi = {}
+    for key, value in res_tf.items():
+        keyw = cosine.text_to_vector(keyword)
+        doc = articles[key] # artikel dengan minimal 4 freq kemunculan
+        # doc = cosine.text_to_vector(value) # artikel di vectorkan
+        score = round(cosine.get_cosine(keyw, doc)*100,4)
+        if score != 0.0:
+            simi[key] = score
+        # print key
+        # print doct[key]
+        # print doc
+    
+    hasil_simi = w4.sortdic(simi, descending=True) # hasil return di zip dan sort
+
+    return hasil_simi
+
+# keyw = "jokowi presiden"
 # keyw = ['saya','yakin','dia','menolak','tembak','sebelum']
 # keyw = ['yang']
 # print text_exam()
+# print findSim(keyw, path)
 # print test_similarity(dob(path),keyw)
+# doct = dob(path) 
+# print doct['lf.txt']
